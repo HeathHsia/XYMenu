3d56670347b8899522dcafc7cd34622506862b17
HeathHsia <firehsia1204@gmail.com>
2018-02-02 15:02:58 +0800

    Init Swift Demo

---
+++ XYMenuSwiftDemo/XYMenuSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XYMenuSwiftDemo
+//
+//  Created by FireHsia on 2018/2/2.
+//  Copyright © 2018年 FireHsia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
