94376bf1fbc31a167b7c3f0ea4aecb33257320b7
HeathHsia <firehsia1204@gmail.com>
2018-02-02 15:49:28 +0800

    commit Swift Code

---
+++ XYMenuSwiftDemo/XYMenuSwiftDemo/Class/XYMenuItem.swift
@@ -0,0 +1,21 @@
+//
+//  XYMenuItem.swift
+//  XYMenuSwiftDemo
+//
+//  Created by FireHsia on 2018/2/2.
+//  Copyright © 2018年 FireHsia. All rights reserved.
+//
+
+import UIKit
+
+class XYMenuItem: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
